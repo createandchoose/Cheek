48169af998e9ad16830cf617b4522bb3dbb435a0
Artem Boytunov <aboytunov@gmail.com>
2022-11-24 15:13:54 +0900

    main

---
+++ js/hackatons.js
@@ -0,0 +1,58 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyCRRohu86ty2HMDfoaNyj3YoCx6DWs7Y6M",
+	authDomain: "cheek-d4b6d.firebaseapp.com",
+	databaseURL: "https://cheek-d4b6d-default-rtdb.firebaseio.com",
+	projectId: "cheek-d4b6d",
+	storageBucket: "cheek-d4b6d.appspot.com",
+	messagingSenderId: "272145172180",
+	appId: "1:272145172180:web:37db41f797ecdf81ab9d41"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
+
+
+  window.onload=function(){
+		this.getdata();
+  }
+  
+  
+  function getdata(){
+		firebase.database().ref('hackaton/').once('value').then(function(snapshot){
+		  //get your posts div
+		  var posts_div=document.getElementById('posts');
+		  //remove all remaining data in that div
+		  posts.innerHTML="";
+		  //get data from firebase
+  
+		  var data=snapshot.val();
+		  console.log(data);
+  
+		  for(let[key,value] of Object.entries(data)){
+			 
+			 posts_div.innerHTML="<div class=' mt-2 mb-1 align-items-center'>"+
+			 "<div class='card' >"+
+			 "<img src='"+value.imageURL+"' class='clients-img1 align-items-center aos-init aos-animate''>"+
+			 "<div class='card-body'><p class='card-text'>"+value.text+"</p>"+
+			 "<p class='card-text-small'>"+value.description+"</p>"+
+		    "<p class='card-text'>"+value.date+"</p>"+
+			 "<button class='btn btn-danger'  onclick='submit(this.id)'>Учавствовать</button>"+
+
+			 "</div></div></div>"+posts_div.innerHTML;
+  
+  
+  }
+		
+		});
+  }
+  
+  
+
+  
+
+  function submit(){
+	window.open('submit.html');
+  }
+
+
+
---
+++ js/submit.js
@@ -0,0 +1,59 @@
+const firebaseConfig = {
+
+	apiKey: "AIzaSyCRRohu86ty2HMDfoaNyj3YoCx6DWs7Y6M",
+	authDomain: "cheek-d4b6d.firebaseapp.com",
+	databaseURL: "https://cheek-d4b6d-default-rtdb.firebaseio.com",
+	projectId: "cheek-d4b6d",
+	storageBucket: "cheek-d4b6d.appspot.com",
+	messagingSenderId: "272145172180",
+	appId: "1:272145172180:web:37db41f797ecdf81ab9d41"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+ e.preventDefault();
+
+ var name = getElementVal("name");
+ var numberid = getElementVal("numberid");
+ var category = getElementVal("category");
+ 
+
+ saveMessages(name, numberid, category);
+
+ //   enable alert
+ document.querySelector(".alert").style.display = "block";
+
+ //   remove the alert
+ setTimeout(() => {
+	document.querySelector(".alert").style.display = "none";
+ }, 3000);
+
+ //   reset the form
+
+}
+
+const saveMessages = (name, numberid, category) => {
+ 
+ var newContactForm = contactFormDB.push();
+
+ var newContactForm = firebase.database().ref("contactForm").child(category + '/' + numberid);
+
+ newContactForm.set({
+	name: name,
+	numberid: numberid,
+	category: category,
+ });
+};
+
+
+const getElementVal = (id) => {
+ return document.getElementById(id).value;
+};
